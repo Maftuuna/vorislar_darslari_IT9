56fc9f502d82ba6f88366924d8a1389e471a4cc0
Maftuuna <niyozmetovmuzaffar11@gmail.com>
2025-10-09 16:30:28 +0500

    ghgfff

--- python.py
+++ python.py
@@ -1,5 +1,34 @@
-print ("print: Chiqarish operatori,Masalan:")
-a=5
-b=8
-s=a*b
-print(s)
+# Bugundan Python bilan ishlaymiz
+# Print() konsolga malumot  chiqarish buyrugi
+
+print('Salom   barchangizga')
+print("mayli Salom dunyo")
+print(""" Bu nima degani""")
+
+
+# Arifmetik amallar 
+# Qoshish 
+
+a = 12
+b = 34
+c = a + b 
+print ('c =', c) 
+
+# Ayirish 
+v = a - b
+print('v =',v)
+
+# Kopaytirish
+
+m = a * b # koyaptirsh
+l = a**b # darajaga kotarish
+print('m=',m)
+print('l=',l)
+
+# Bolish 
+g = 5
+p1 = g / 2 # haqiqiy qismini olish
+p2 = g// 2 # butun qismini oladi
+
+print('p1= ',p1)
+print ('p2 =',p2)
