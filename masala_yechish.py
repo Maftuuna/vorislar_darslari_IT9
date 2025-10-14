3cdd51caeee7f4a7fd08489f52ad8cb3cd98f1e3
Maftuuna <niyozmetovmuzaffar11@gmail.com>
2025-10-14 15:36:37 +0500

    uchinchidars

---
+++ masala_yechish.py
@@ -0,0 +1,10 @@
+
+import math 
+#A va B sonalari berilgan Ularning qiymatlarini almashtirib rezultat oling 
+a = int(input("A sonini kiriting: "))
+b= int(input("B sonini kiriting: "))
+
+print("A sonini kiriting: A =",b)
+print("B sonini kiriting: B =",a)
+
+
