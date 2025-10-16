a4a4d6db8f57fbe8b9ed12c5705cfee637b39eb3
Maftuuna <niyozmetovmuzaffar11@gmail.com>
2025-10-16 15:57:42 +0500

    mm

--- masala_yechish.py
+++ masala_yechish.py
@@ -1,10 +1,23 @@
 
 import math 
 #A va B sonalari berilgan Ularning qiymatlarini almashtirib rezultat oling 
-a = int(input("A sonini kiriting: "))
-b= int(input("B sonini kiriting: "))
+#a = int(input("A sonini kiriting: "))
+#b= int(input("B sonini kiriting: "))
 
-print("A sonini kiriting: A =",b)
-print("B sonini kiriting: B =",a)
+#print("A sonini kiriting: A =",b)
+#print("B sonini kiriting: B =",a)
 
 
+#Nolga teng bolmaagan qiymat kiriting
+
+a=int(input("A sonini kiriting:"))
+b=int(input("B sonini kiriting:"))
+
+print("A sonini kvadrati",a)
+print("B sonining kvadrati",b)
+print("yigindisi:",a+b)
+print("ayirmasi",a-b)
+print("kvadrati",a**b)
+print("bolinmasi",a/b)
+
+ 
