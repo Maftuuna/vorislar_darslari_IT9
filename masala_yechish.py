14ee27d9262da67e366fcb40b0c7a7cd37a70515
Maftuuna <niyozmetovmuzaffar11@gmail.com>
2025-10-16 16:26:25 +0500

    mm

--- masala_yechish.py
+++ masala_yechish.py
@@ -10,14 +10,33 @@ import math
 
 #Nolga teng bolmaagan qiymat kiriting
 
-a=int(input("A sonini kiriting:"))
-b=int(input("B sonini kiriting:"))
+#a=int(input("A sonini kiriting:"))
+#b=int(input("B sonini kiriting:"))
 
-print("A sonini kvadrati",a)
-print("B sonining kvadrati",b)
-print("yigindisi:",a+b)
-print("ayirmasi",a-b)
-print("kvadrati",a**b)
-print("bolinmasi",a/b)
+#print("A sonini kvadrati",a)
+#print("B sonining kvadrati",b)
+#print("yigindisi:",a+b)
+#print("ayirmasi",a-b)
+#print("kvadrati",a**b)
+#print("bolinmasi",a/b)
 
+ #Tekislikdagi berilgan ikkita nuqta(x1;x2)(y1;y2)orasidagi masofani toping
  
+#x1=int(input("x1 koordinataga qiymat kiriting:"))
+#x2=int(input("x2 koordinataga qiymat kiriting:"))
+#y1=int(input("y1 koordinataga qiymat kiriting:"))
+#y2=int(input("y1 koordinataga qiymat kiriting:"))
+
+
+#print("ikki nuqa orasidagi masofa:",math.sqrt((x2-x1) **2 +9*(y2-y1) **2))
+
+a=int(input("A soni qiymatini kiriting:"))
+b=int(input("B soni qiymatini kiriting:"))
+
+print("A sonining moduli:",(a))
+print("B sonining moduli:",(b))
+print("yig'indisi:",a+b)
+print("ayirmasi:",a-b)
+print("ko'paytmasi",a*b)
+
+
