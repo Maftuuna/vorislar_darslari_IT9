431254ff6fa58cc00ad6b34c107da78ab4a856d3
Maftuuna <niyozmetovmuzaffar11@gmail.com>
2025-10-18 16:13:27 +0500

    metod

---
+++ string.metods.py
@@ -0,0 +1,49 @@
+# String , methods, f"" string
+
+# ism='maftuna'
+# familiya='madaminova'
+# otchestva='mazaffarovna'
+# FIO = f"Assalomu alaykum {familiya} {ism} { otchestva}!"
+
+# print(FIO)
+# print(f"Assalomu alaykum {familiya.title} {ism.title} { otchestva.title}!")
+
+
+# meva = 'anor,olma,anjir'
+
+# txt  = f" maftuna hello {meva} yisanmi"
+# x= txt.capitalize()
+# print(x)
+
+ # lower, casefold barcha matn ichidagi katta harflarni kichkina yozadi
+# ism = 'Ollayorova Matluba'
+# txt = "Hello,Matluba"
+# x = txt.casefold()
+# print(x)
+# print(ism.lower())
+
+# txt = "Apple"
+
+# x = txt.center(150)
+# print(x)
+
+# count()String ma'lumotda nechta ma'lum harf yoki soz borligini aniqlaydi 
+# txt="I love apples,apple are my favorite fruit"
+# x = txt.count("e")
+# print(x)
+
+# endwith() String ma'lumot ma'lum bir so'z yoki harf bn tugashini aytadi
+# txt = "Hello,welcome to my word."
+# x = txt.endswith(".")
+# print(x)
+
+# expandtabs() string ma'lumotdagi tab belgilarini bosh joy bn almashtiradi
+
+# txt= "M\taftu\na"
+# x= txt.expandtabs(0)
+# print(x)
+
+# txt = "salom dunyo, nima gaplar. "
+# x = txt.find("dunyo")
+
+# print(x)
