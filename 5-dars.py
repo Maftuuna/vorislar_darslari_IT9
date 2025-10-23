e621cb4b390776a1050a575b63acdfd36ac2331e
Maftuuna <niyozmetovmuzaffar11@gmail.com>
2025-10-23 16:24:38 +0500

    mm

---
+++ 5-dars.py
@@ -0,0 +1,63 @@
+""""
+21.10.2025
+Python list operations
+"""
+
+# ism = "maftuna"
+# uy_manzili_= "xorazm viloyati,xonqa tumani"
+
+# print(ism.upper())
+# print(uy_manzili_.title())
+
+# #Python da List(royxatlar) bilan ishlash
+
+# hayvonlar=["mushuk","ot","kuchuk","yo'bars", "sigir"]
+# oila=["ota",24,"ona",22,"opa",15,"singil",11,"uka",8]
+# mashinalar=["bmw","malibu","gentra","cobalt","traker"]
+
+
+# hayvonlar=["mushuk","ot","kuchuk","yo'bars", "sigir"]
+# #index        0       1    2         3         4
+
+# print(hayvonlar)
+
+# qiymat= hayvonlar[1]
+
+# print(qiymat)
+
+
+
+# mashinalar=["bmw","malibu","gentra","cobalt","traker"]
+# #index        0      1        3        4        5
+
+
+# print(mashinalar)
+# qiymat=mashinalar[0]
+# print(qiymat)
+# print(mashinalar[0].title())
+
+# print(mashinalar[-2].upper())
+
+
+# mevalar=('olma','anor','behi','nok','apelsin','limon','laym','banan')
+# #index    0       1      2      3     4          5      6       7
+
+# print(mevalar)
+# qiymat=(mevalar[-4] .upper())
+# print(qiymat)
+
+
+# IT_oquvchilar = []
+
+# IT_oquvchilar.append("Matluba")
+# IT_oquvchilar.append("Maftuna")
+# IT_oquvchilar.append("Mashxura")
+# IT_oquvchilar.append("Sharofat")
+# IT_oquvchilar.append("Madina")
+# IT_oquvchilar.append("Mehrinoz")
+# print(IT_oquvchilar)
+
+
+# IT_oquvchilar.insert(2,"Mehrojbek")
+# IT_oquvchilar.insert(4,"Mehribon")
+# print(IT_oquvchilar)
---
+++ 6.dars.py
@@ -0,0 +1,25 @@
+# Royxatlar list takrorlash
+
+
+dugonalarim=("Matluba","Mohinur","Dilnura","Zebo","Mushtariy","Madina")
+print(dugonalarim)
+ism=(dugonalarim[0].title())
+print(ism)
+
+nomi=("Matluba nima gaplar?")
+print(nomi)
+
+nomi1=("Mohinur bormisan? ")
+print(nomi1)
+
+nomi2=("Dilura nima gaping bolsa bemalol aytaver")
+print(nomi2)
+
+nomi3=("Zebo o'ylagan niyatizga yeting")
+print(nomi3)
+
+nomi4=("Mushtariy maktab qanday bolyapti?")
+print(nomi4)
+
+nomi5=("Madina qachon menga gul berasan?")
+print(nomi5)
