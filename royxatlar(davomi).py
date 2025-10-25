f20abdb3d7d59ac599bb034f4e209524f1a30d4a
Maftuuna <niyozmetovmuzaffar11@gmail.com>
2025-10-25 16:39:46 +0500

    royxatlardavomi

---
+++ royxatlar(davomi).py
@@ -0,0 +1,60 @@
+"""
+25.10.2025
+Royxatlar bilan ishlash.Methodlar va function lar bilan ishlash
+methods(.sort())
+function(sum(),min(),max(),sorted(),list(),)
+Royxatlardan nusxa olish
+Tuple() ozgarmas royhat
+"""
+
+# davlat = ["Uzbekiston","Braziliya","Turkiya","Korea","Amerika","Ispaniya"]
+# mashinalar = list(("BMW","Malibu","Jentra", "Cobalt", "Damas"))
+# sonlar = list ((2,1,7,0,15))
+
+
+
+# print(davlat)
+# print(mashinalar)
+
+
+# sonlar.sort(reverse=True)
+# print(sonlar)
+
+
+# sonlar1= list (range(0,200))
+# print(sonlar1)
+
+# sonlar2 = list(range(3,89, 2))
+# print(sonlar2)
+# print(len(sonlar2))
+
+
+# sonlar3=[89,-98,63,15,-21,567,45,-345,56,-235,589,-370,90]
+
+# minn = min(sonlar3)
+# maxx = max(sonlar3)
+
+# print(max(sonlar3))
+# print(min(sonlar3))
+
+# print(sum(sonlar3))
+
+
+davlat = ["Uzbekiston","Braziliya","Turkiya","Korea","Amerika","Ispaniya"]
+
+
+country = davlat[:]
+
+print("country nomli royxat:",country)
+print("davlat nomli royxat:",davlat)
+
+country.insert(0,"Istanbul")
+print(country)
+
+
+toqsonlar = list(range(1,24,2))
+print("Asl korinishi:",toqsonlar)
+print("1 dan 7 gacha : ",toqsonlar[0:4])
+print(toqsonlar[:])
+
+
