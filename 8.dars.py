687e0136f4effc683b17c8f483514634bbd99db1
Maftuuna <niyozmetovmuzaffar11@gmail.com>
2025-11-01 16:33:05 +0500

    mmm

--- 8.dars.py
+++ 8.dars.py
@@ -20,4 +20,13 @@ oquvchilar =[ "maftuna", "matluba","mashxura", "sharofat","mohinur","dilnura"]
 # for son in raqamlar :
 #    print(f"{son} ning kvadrati : {son**2} ga teng")
    
-sonlar =list(range(30,91))
+# sonlar =list(range(30,91))
+# for son in sonlar:
+# print(f"{son}/9 ning qiymati : {son/9} ga teng")
+
+sonlar2 = []
+
+sonlar = list (range(27,54,7))
+for son in sonlar:
+  sonlar2.append(son**4) 
+print(f"4-darajaga oshan royhat{sonlar2}")
