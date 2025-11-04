2c6d75759f7b209468931a51fa8f61d35b0c69fd
Maftuuna <niyozmetovmuzaffar11@gmail.com>
2025-11-04 16:29:08 +0500

    masala ishlash

--- masala_ishlash.py
+++ masala_ishlash.py
@@ -1,8 +1,22 @@
 #   Birinchi masala. k va n butun sonlar berilgan(n > 0).k sonini n marta chiqaruvchi programma tuzilsin
-k = int(input("k sonini kiriting: "))
-n = int(input("n sonini kiriting: "))
-for i in range(1,n+1):
-    print(f"{i}-sikl : {k}")
+# k = int(input("k sonini kiriting: "))
+# n = int(input("n sonini kiriting: "))
+# for i in range(1,n+1):
+#     print(f"{i}-sikl : {k}")
     
     
-# Ikkinchi masala.  
+# Ikkinchi masala.  a va b butun sonlari berilgan (a < b) a va  b sonlari orasidagi barcha butun sonlarni (a va b ni ham) chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi programma tuzilsin.(a va b ham chiqsin)
+# a = int(input(" a sonini kiriting : "))
+# b = int(input(" b sonini kiriting : "))
+# son = 0
+# for i in range(a,b+1):
+#    son += 1 #son = son +1
+#    print(i)
+# print(f"{a} dan {b} gacha {son} ta son bor")
+# Uchinchi masala. a va b butun sonlari berilgan(a < b). a va b sonalri orasidagi barcha butun sonlar yig'indisini hisoblovchi programma tuzilsin.(a va b ham chiqsin)
+# a = int(input(" a sonini kiriting : "))
+# b = int(input(" b sonini kiriting : "))
+# yigindi = 0
+# for i in range(a,b+1):
+#     yigindi += i
+# print(f"{a} dan {b} gacha sonlar yig'indisi: {yigindi}")
