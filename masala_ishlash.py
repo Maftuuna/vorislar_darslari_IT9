c698f46e394600b7589e95281e3f4e1bf41532f5
Maftuuna <niyozmetovmuzaffar11@gmail.com>
2025-11-04 15:50:48 +0500

    royxat

---
+++ masala_ishlash.py
@@ -0,0 +1,8 @@
+#   Birinchi masala. k va n butun sonlar berilgan(n > 0).k sonini n marta chiqaruvchi programma tuzilsin
+k = int(input("k sonini kiriting: "))
+n = int(input("n sonini kiriting: "))
+for i in range(1,n+1):
+    print(f"{i}-sikl : {k}")
+    
+    
+# Ikkinchi masala.  
