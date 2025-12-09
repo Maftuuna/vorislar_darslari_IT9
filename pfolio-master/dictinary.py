e4aeadf25776823a0daacd8cdd1f3fb314b89795
Maftuuna <niyozmetovmuzaffar11@gmail.com>
2025-12-09 16:32:10 +0500

    dictionary

---
+++ pfolio-master/dictinary.py
@@ -0,0 +1,21 @@
+# ismlar ={
+#     "Matluba","Dilfuza","Nilufar","Gulchehra","Shahnoza",
+# }
+# thisdist = {
+#     "brand" : "Ford",
+#     "model" : "Mustang",
+#     "year" : 1964 
+# }
+# print(thisdist)
+maftuna ={
+    'ism': "Maftuna",
+    'yosh':15,
+    'familiya':'Madaminova ',
+    'manzil':'Qoraqosh',
+    'maktab':'1 -sonli maktab',
+    'sinf': 9,
+    'qiziqishlari':"fizika",
+    'transport': 'Gentra',
+    'telefon': +998947478525,
+}
+print(f" Mening ismim {maftuna ['ism'].title()}, familiyam {maftuna ['familiya'].title()}, Men  {maftuna ['yosh']} yoshdaman, {maftuna ['maktab']}ning {maftuna ['sinf']} -sinf o'quvchisiman. Menga {maftuna ['qiziqishlari']} fani yoqadi.")
