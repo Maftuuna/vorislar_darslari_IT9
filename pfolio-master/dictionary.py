40409efebe3141db5451d3cb445372a41d12b48f
Maftuuna <niyozmetovmuzaffar11@gmail.com>
2025-12-13 16:20:54 +0500

    dictionary

---
+++ pfolio-master/dictionary.py
@@ -0,0 +1,101 @@
+talaba1 ={
+    "ism":"kamolbek",
+    "familiya" :"rozmamatov",
+    "maktab":"1-sonli maktab",
+    "qiziqishlari":"futbol",
+    
+}
+talaba2 = {
+    "ism":"boboxon",
+    "familiya" :"jumaboyev",
+    "maktab":"12-sonli maktab",
+    "qiziqishlari":"It darslari",
+}
+talaba3 = {
+    "ism":"elbek",
+    "familiya" :"jabborov",
+    "maktab":"46-sonli maktab",
+    "qiziqishlari":"dasrturlash",
+
+}
+talaba4 ={
+    "ism":"ixtiyor",
+    "familiya" :"baxtiyorov",
+    "maktab":"26-sonli maktab",
+    "qiziqishlari":"futbol",
+
+}
+talaba5={
+    "ism":"xolbek",
+    "familiya" :"qurbonboyev",
+    "maktab":"1-sonli maktab",
+    "qiziqishlari":"futbol",
+}
+talaba6={
+    "ism":"ilyos",
+    "familiya" :"qochqorboyev",
+    "maktab":"1-sonli maktab",
+    "qiziqishlari":"futbol",
+}
+talaba7={
+    "ism":"umitsz",
+    "familiya" :"matyoqubov",
+    "maktab":"1-sonli maktab",
+    "qiziqishlari":"telefon bilan o'ynash",
+}
+talaba8={
+    "ism":"zuhriddin",
+    "familiya" :"qadamboyev",
+    "maktab":"26-sonli maktab",
+    "qiziqishlari":"informatika",
+}
+talaba9={
+    "ism":"baxtiyor",
+    "familiya" :"ozodov",
+    "maktab":"1-sonli maktab",
+    "qiziqishlari":"futbol,IT darslar",
+}
+talaba10={
+    "ism":"muhammadali",
+    "familiya" :"ibragimov",
+    "maktab":"1-sonli maktab",
+    "qiziqishlari":"dasturlash, ingliz tili",
+}
+talaba11={
+    "ism":"maftuna",
+    "familiya" :"madaminova",
+    "maktab":"1-sonli maktab",
+    "qiziqishlari":"IT darslar,ingliz tili,fizika",
+}
+talaba12={
+    "ism":"matluba",
+    "familiya" :"ollayorova",
+    "maktab":"1-sonli maktab",
+    "qiziqishlari":"IT darslar,ingliz tili,rus tili",
+}
+talaba13={
+    "ism":"sharofat",
+    "familiya" :"jumamurodova",
+    "maktab":"1-sonli maktab",
+    "qiziqishlari":"Ingliz tili,koreys tili",
+}
+talaba14={
+    "ism":"mashxura",
+    "familiya" :"shomurotova",
+    "maktab":"1-sonli maktab",
+    "qiziqishlari":"IT darslar,ingliz tili",
+}
+
+
+
+malumot_tiplari= {
+    'str': 'srting',
+    'int': 2025,
+    'float': 20.25,
+    'bool': True,
+    'list':['malibu,cobalt,tracker'],
+    'tuple':('pi','g','k')
+}
+
+
+print(talaba12.familiya.title())
